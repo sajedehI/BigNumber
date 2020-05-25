2579c643259da10f73d47f5e14acd30ef8a1665f
pc <mariavasajedeh@gmail.com>
2020-05-25 20:52:41 +0430

    Add main

--- main.cpp
+++ main.cpp
@@ -21,16 +21,30 @@ int main() {
 //    x = -y;
 //    BigNumber z = -y;
 //    cout << (x != y) << endl;
-    BigNumber myBig1 = -17319;
-    BigNumber myBig2 = "0000173";
+   // BigNumber myBig1 = -17319;
+  //  BigNumber myBig2 = "0000173";
     //cout << BigNumber::unsignedLessOrEqual(myBig1, myBig2) << endl;
     //BigNumber a = BigNumber::unsignedSubtract(myBig1, myBig2);
-    cout << (myBig1>>3) << "\t" << (myBig1>>3).getSign() << endl;
+ //   cout << (myBig1>>3) << "\t" << (myBig1>>3).getSign() << endl;
 //    try{
 //        cout << BigNumber::unsignedSubtract(myBig1, myBig2) << endl;
 //    }
 //    catch( out_of_range e){
 //        cout << e.what() << endl;
 //    }
+        BigNumber b1 = 47;
+        int8_t b2 = 5;
+        BigNumber b3 = 256;
+        BigNumber b4 = 1145;
+        BigNumber b5 = 1247896;
+        BigNumber b6 = 14547856;
+        BigNumber b7 = 295;
+        cout << "shift to left : " << (b4<<5) << endl;
+        cout << "mult by one digit : " << MyBigNumber :: multByOneDigit(b7,b2) << endl;
+        cout << "subtract : " << (b3 - b1) << endl;
+        cout << "++ : " << b6++ << endl;
+        cout << "-- : " << b5-- << endl;
+
+
     return 0;
 }
